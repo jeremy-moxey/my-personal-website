55b268a8012a4c7f5de7841abe06afee3af630f3
Jeremy Moxey <jermoxey@gmail.com>
2022-05-02 15:55:13 -0700

    links to GitHub, pointer over images

--- script.js
+++ script.js
@@ -4,15 +4,36 @@ h2.textContent = "Summary";
 document.querySelector("body").appendChild(h2)
 
 //Clickable LinkedIn Logo
-const logo = document.getElementById("logo")
-function clickAlert() {
+const logoLi = document.getElementById("linkedin")
+function clickAlertLi() {
     alert("Going to LinkedIn")
 }
-logo.addEventListener('click', clickAlert)
-const linkedIn = "https://linkedin.com/in/jeremy-moxey/"
+logoLi.addEventListener('click', clickAlertLi)
 
-function navigate() {
+//Clickable GitHub Logo
+const logoGh = document.getElementById("octocat")
+function clickAlertGh() {
+    alert("Going to GitHub")
+}
+logoGh.addEventListener('click', clickAlertGh)
+
+//LinkedIn Navigation
+function navigateToLinkedIn() {
     open("https://linkedin.com/in/jeremy-moxey/")
 }
-logo.addEventListener('click', navigate)
+logoLi.addEventListener('click', navigateToLinkedIn)
 
+//GitHub Navigation
+function navigateToGitHub() {
+    open("https://github.com/jeremy-moxey/my-personal-website")
+}
+logoGh.addEventListener('click', navigateToGitHub)
+
+//Pointer style cursor for linked images
+const logoAll = document.querySelectorAll(".logo")
+for (const logo of logoAll) {
+    logo.addEventListener("mouseover", function(event) {
+        // highlight the mouseover target
+        console.log(event)
+        logo.style.cursor = "pointer";})
+}
