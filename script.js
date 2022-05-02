8cabcbb50fa1ba627e6a1f10aa1add3304af5a52
Jeremy Moxey <jermoxey@gmail.com>
2022-05-02 14:20:30 -0700

    footer with linked img

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "Summary";
 // Add h2 to body
 document.querySelector("body").appendChild(h2)
+
+//Clickable LinkedIn Logo
+const logo = document.getElementById("logo")
+function clickAlert() {
+    alert("Going to LinkedIn")
+}
+logo.addEventListener('click', clickAlert)
+const linkedIn = "https://linkedin.com/in/jeremy-moxey/"
+
+function navigate() {
+    open("https://linkedin.com/in/jeremy-moxey/")
+}
+logo.addEventListener('click', navigate)
+
