cd0101ef07668c845732a00b6ea1e4aeed28ca8d
Jeremy Moxey <jermoxey@gmail.com>
2022-05-03 23:05:36 -0700

    Display home content by default, click to show content of other tab

--- script.js
+++ script.js
@@ -1,21 +1,75 @@
+////////// CONTENT///////////////
+
+
+// HOME
 const h2 = document.createElement("h2");
 h2.textContent = "Currently Working On...";
 h2.style.fontSize = "14pt"
 // Add h2 to body
-document.querySelector("body").appendChild(h2)
+//document.querySelector("#content").appendChild(h2)
+document.querySelector("#home-default").appendChild(h2)
+
+//Skills & Tools
+const h2Skills = document.createElement("h2");
+h2Skills.textContent = "Skills and Current Tech Stack";
+h2Skills.style.fontSize = "14pt"
+// Add h2 to body
+//document.querySelector("#content").appendChild(h2)
+document.querySelector("#skills-content").appendChild(h2Skills)
 
 ////////// MENU BAR EVENTS /////////////////////////
-const menuBar = document.querySelectorAll("li")
+const menuBar = document.querySelectorAll(".tablinks")
 for (const item of menuBar) {
     item.addEventListener('mouseover', function(e) {
-        console.log(e)
-        item.style.backgroundColor = "black"
+        //console.log(e)
+        item.style.backgroundColor = "salmon"
+        item.style.cursor = "pointer"
     }) 
     item.addEventListener('mouseout', function(e) {
-        console.log(e)
+        //console.log(e)
         item.style.backgroundColor = "grey"
     }) 
 }
+
+function showContent() {
+    let tabContent
+    const tabLink = document.querySelector(".active")
+    if (tabLink.id === "home-button") {
+        tabContent = document.querySelector("#home-default")
+    } 
+    else if (tabLink.id === "skills-button") {
+        tabContent = document.querySelector("#skills-content")
+    }
+    else {console.log("nothing")}
+    tabContent.style.display = "block"
+}
+//Hide all content
+const tabContent = document.getElementsByClassName("tab-content")
+for (const item of tabContent) {
+    item.style.display = "none"
+}
+
+//Show content if clicked
+const tabLinks = document.getElementsByClassName("tablinks")
+for (const item of tabLinks) {
+    item.addEventListener('click', function () {
+        console.log("click")
+        item.className += " active"
+        console.log("active")
+        showContent()
+        item.className = item.className.replace(" active", "")
+        
+    })
+    //item.children.style.display = "none" //first hide all content
+
+    //item.children.addEventListener('click', function (e) {
+
+    //    console.log(e)
+    //    item.children.style.display = "block"
+    }
+
+document.getElementById("home-button").click()
+
 
 
 /*const mainMenu = document.createElement("ul")
